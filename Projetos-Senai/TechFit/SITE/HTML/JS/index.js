5979dcee677d86e94011371276bcf0339cedb4d8
matheus130607 <matheusmalaman2014@gmail.com>
2025-10-16 14:34:11 -0300

    16.10.25

---
+++ Projetos-Senai/TechFit/SITE/HTML/JS/index.js
@@ -0,0 +1,24 @@
+// Pagina Usuario
+
+        function Login() {
+            window.location.href = "login.html"; 
+        
+        }
+
+        function plano() {
+            location.href ="#planos-container";
+        }
+
+        function faleconosco() {
+            location.href ="#links-footer"
+        }
+
+        function loja() {
+            window.location.href = "loja.html"
+        }
+
+        function home() {
+            window.location.href = "Pag_Inicial_CL.html"
+        }
+
+       
